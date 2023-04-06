bdbb6c36014a53ee8444055e9abb5beeb0465fbd
karol204 <krogalski204@gmail.com>
2023-04-06 22:48:41 +0100

    setup teamplate

---
+++ src/components/BlogPostMin/BlogPostMin.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogPostMin = () => {
+  return <div>BlogPostMin</div>;
+};
+
+export default BlogPostMin;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="main">
+      <img src="" className="logo" />
+      <div className="links-container">
+        <a>Code</a>
+        <a>Design</a>
+        <a>Lifestyle</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
